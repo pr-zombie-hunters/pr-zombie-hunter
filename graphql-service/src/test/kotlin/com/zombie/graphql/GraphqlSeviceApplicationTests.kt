f5793fee6630086dfb552406b0269d029ce13b9e
yoonseo-dev <yoonseo0127@gmail.com>
2026-05-30 11:50:21 +0900

    프로젝트 초기 구조 세팅

---
+++ graphql-service/src/test/kotlin/com/zombie/graphql/GraphqlSeviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zombie.graphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlSeviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
