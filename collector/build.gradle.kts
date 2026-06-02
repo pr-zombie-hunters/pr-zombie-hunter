9f65831deb13d3663bac6233defb982330d6a77b
HYcho13 <GitHubhycho13@gmail.com>
2026-06-02 21:56:03 +0900

    feat: GitHub Webhook PR 이벤트 수신 및 Secret 검증 구현 [SCRUM-60]

--- collector/build.gradle.kts
+++ collector/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("tools.jackson.module:jackson-module-kotlin")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
